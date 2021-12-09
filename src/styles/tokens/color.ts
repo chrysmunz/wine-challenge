9bcf4e556b04c2526977996136998ee7bb8f3e68
chrysmunz <chrys.munz@hotmail.com>
2021-12-09 17:44:41 -0300

    feat: :sparkles: new button component

--- src/styles/tokens/color.ts
+++ src/styles/tokens/color.ts
@@ -1,4 +1,5 @@
 export default {
   white: '#FFFFFF',
-  black: '#000000'
+  black: '#000000',
+  green: '#7EBC43'
 } as const;
--- storybook/storyLoader.ts
+++ storybook/storyLoader.ts
@@ -4,9 +4,11 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+  require('../src/components/Button/Button.stories');
 }
 
 const stories = [
+  '../src/components/Button/Button.stories',
 ];
 
 module.exports = {
