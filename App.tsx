e9be7adcea7d2e94b7781001fa7bb0057fdd0e53
chrysmunz <chrys.munz@hotmail.com>
2021-12-13 19:02:06 -0300

    feat: :sparkles: added theme provider

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 
 import store from './src/store';
+import { theme } from './src/styles/theme';
 import Router from './src/router/Router';
 
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <Router />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    </ThemeProvider>
   );
 };
 
